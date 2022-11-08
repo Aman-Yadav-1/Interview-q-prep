f0cb2f587bd295ffcf0afcd51f51bbd45d5e10b6
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 12:22:46 +0530

    p7

--- patterns/p7.java
+++ patterns/p7.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
 public class p7 {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= i;j++){
+                if(i == j){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }    
 }
