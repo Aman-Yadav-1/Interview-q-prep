a76e59f6927e39de3bb7af7aecdfbbc525721597
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 12:54:40 +0530

    p5

--- patterns/p2.java
+++ patterns/p2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class p1_pyramid {
+public class p2{
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
 
-        for(int i = 1;i <= n;i++){
-            for(int j = 1;j<=i;j++){
+        for(int i = n;i >= 1;i--){
+            for(int j = 1;j <= i;j++){
                 System.out.print("*\t");
             }
             System.out.println();
