58fa6aa10293ce1dd4711fd99adf5c7572c38bd1
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 12:33:06 +0530

    p8

---
+++ patterns/p8.java
@@ -0,0 +1,26 @@
+//printed as
+        /*
+     * 
+   *
+*/
+import java.util.Scanner;
+
+public class p8 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++){
+                if(i + j == n + 1){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
