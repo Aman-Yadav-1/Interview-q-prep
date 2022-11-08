0b57b988121bf01c216cf44e456caef467bb4827
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 14:52:33 +0530

    fibonacci

--- Basics/fibonacci.java
+++ Basics/fibonacci.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class fibonacci{
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
+
         int n = scan.nextInt();
         int a = 0;
         int b = 1;
-        for(int i=0;i <= n;i++){
-            System.out.println(a);
+
+        for(int i = 0;i <= n;i++){
+            System.out.print(a + " ");
             int c = a + b;
             a = b;
             b = c;
