8b0f86eafe77debefd3629072d276867ee94ff81
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 14:41:21 +0530

    fibonacci

--- Basics/fibonacci.java
+++ Basics/fibonacci.java
@@ -1,5 +1,20 @@
 package Basics;
+import java.util.Scanner;
 
 public class fibonacci {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n1 = 0;
+        int n2 = 1; 
+        int n3 = scan.nextInt();
+        int count  = n3;
+        System.out.print(n1 + " " + n2);
+
+        for(int i = 2;i < count;++i){
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
 }
