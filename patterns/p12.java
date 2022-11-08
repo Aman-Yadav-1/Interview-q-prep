34619ea35cb8884c15ce7fe776980edc7d2fcaec
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-08 12:53:10 +0530

    p12

--- patterns/p12.java
+++ patterns/p12.java
@@ -1,27 +1,25 @@
-//pattern for numbers
-
-/* 
-  as
-  1 
-  2 3
-  4 5 6
-  7 8 9 10
+/*
+ pattern as
+ 1
+ 1 2
+ 1 2 3
+ 1 2 3 4
  */
+
 import java.util.Scanner;
 
-public class p11 {
+public class p12 {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
-        
+
         int n = scan.nextInt();
-        int val = 1;
 
         for(int i = 1;i <= n;i++){
             for(int j = 1;j <= i;j++){
-                System.out.print(val + "\t");
-                val++;
+                System.out.print(j + "\t");
             }
+
             System.out.println();
         }
-    }
+    }    
 }
