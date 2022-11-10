fd7be08e67dee1fad1deb3b178d2e49d7210ec8b
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-10 22:44:00 +0530

    fibonacci

--- Basics/fibonacci.java
+++ Basics/fibonacci.java
@@ -11,7 +11,7 @@ public class fibonacci{
         int n = scan.nextInt();
         int a = 0;
         int b = 1;
-
+        
         for(int i = 1;i <= n;i++){
             System.out.print(a + " ");
             int c = a + b;
